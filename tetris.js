5ba99aebc848c6be11bded58e4a040e9504cfc97
EuphoriaBot <dimasfirja80@gmail.com>
2026-07-07 22:31:33 +0800

    Draw Background

--- tetris.js
+++ tetris.js
@@ -70,6 +70,7 @@ let currentShape;
 let nextShape;
 let score;
 let initialTwoDArr;
+let whiteLineThickness = 4;
 
 let gameLoop = () => {
     setInterval(update, 1000 / gameSpeed);
@@ -77,6 +78,22 @@ let gameLoop = () => {
 };
 
 let update = () => { };
+
+let drawRect = (x, y, width, height, color) => {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, width, height);
+};
+
+let drawBackground = () => {
+    drawRect(0, 0, canvas.width, canvas.height, "#bca0dc");
+    for (let i = 0; i < squareCountX + 1; i++) {
+        drawRect(size * i - whiteLineThickness, 0, whiteLineThickness, canvas.height, "white");
+    }
+
+    for (let i = 0; i < squareCountY + 1; i++) {
+        drawRect(0, size * i - whiteLineThickness, canvas.width, whiteLineThickness, "white");
+    }
+};
 
 let draw = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
